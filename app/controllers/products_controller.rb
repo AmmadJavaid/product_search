b68c3114a6e34eb64787911b3d770f1fced31b58
Ammad <kh.ammad123@gmail.com>
2020-01-26 22:57:57 +0500

    Product: Added product details page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: :show
 
   def index
     @products = Product.search_for_index(params)
+  end
+
+  def show; end
+
+  private
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
+    return redirect_to root_path, alert: "Product not found" if @product.blank?
   end
 end
